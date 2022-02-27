ca8e98d22ec266f637804ca6ddca69b4e8bc370f
dem-elena <dem-ea@yandex.ru>
2022-02-27 23:19:09 +0300

    Анимация модального окна

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,14 +1,21 @@
 const menu = () => {
+    let widthDoc= document.documentElement.clientWidth
     const menuBtn=document.querySelector(".menu")
     const menu=document.querySelector("menu")
     const closeBtn=menu.querySelector('.close-btn')
     const menuItems=menu.querySelectorAll('ul>li>a')
 
     const handleMenu =()=>{ 
-        // if (!menu.style.transform)
-        // {menu.style.transform="translateX(0)"}
-        // else {menu.style.transform=""}
+        widthDoc= document.documentElement.clientWidth
+        
+        if (widthDoc>768){
         menu.classList.toggle('active-menu')
+        }
+        else{
+            if (!menu.style.transform)
+        {menu.style.transform="translateX(0)"}
+        else {menu.style.transform=""}
+        }
     }
 
     menuBtn.addEventListener('click',handleMenu)
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,10 +1,38 @@
 const modal=()=>{
+    let widthDoc= document.documentElement.clientWidth
     const modal =document.querySelector('.popup')
+    const modalContent=modal.querySelector(".popup-content")
     const buttons=document.querySelectorAll('.popup-btn')
     const closeBtn=modal.querySelector('.popup-close')
+    let idInterval
+    let count=0
+    const movePopup=()=>{
+        count++
+        if (count<30){
+            modalContent.style.left=count*2+"%"
+            idInterval=requestAnimationFrame(movePopup)
+        }
+        else {
+            cancelAnimationFrame(idInterval)
+            count=0
+        }
+
+
+    }
     buttons.forEach(btn =>{
-        btn.addEventListener('click',()=>{modal.style.display="block"})
+        btn.addEventListener('click',()=>{
+            widthDoc= document.documentElement.clientWidth
+            //debugger
+            modalContent.style.left="1%"
+            modal.style.display="block"
+            if (widthDoc>768){
+                idInterval=requestAnimationFrame(movePopup)
+            }
+            
+         })
     })
-    closeBtn.addEventListener('click',()=>{modal.style.display="none"})
+    closeBtn.addEventListener('click',()=>
+    {modal.style.display="none"
+    count=0})
 }
 export default modal
