28f90488e3d8dba35819045e93235dc6733ebacb
dem-elena <dem-ea@yandex.ru>
2022-03-01 22:20:13 +0300

    слайдер

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu"
 import modal from "./modules/modal"
 import validate from "./modules/validate"
 import tabs from "./modules/tabs"
+import slider from "./modules/slider"
 
 timer("08 march 2022")
 menu()
 modal()
 validate()
 tabs()
+slider()
